1bc48365e4a850b9780033bbfc566d722848f6d8
Bazsalanszky <balazs@toldi.eu>
2022-03-07 15:43:16 +0100

    Added CicaTeszt

---
+++ train-user/src/test/java/hu/bme/mit/train/user/CicaTeszt.java
@@ -0,0 +1,45 @@
+package hu.bme.mit.train.user;
+
+import hu.bme.mit.train.interfaces.TrainUser;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class CicaTeszt {
+
+
+    CicaGomb cg;
+
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void before() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+        cg = new CicaGomb();
+
+    }
+
+    @Test
+
+    public void cteszt(){
+        cg.megnyom();
+        assertEquals("Ne nyopmkodd szegény macskát!\n", outContent.toString());
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+}
