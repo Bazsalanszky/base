662e5f7f2f1b44260b2c74439a9d50b9ff652bc6
Bazsalanszky <balazs@toldi.eu>
2022-05-16 17:02:57 +0200

    Added testcases

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,53 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    private TrainSensor sensor;
+    private TrainController controller;
+    private TrainUser user;
+
     @Before
-    public void before() {
-        // TODO Add initializations
+    public void init(){
+        user = mock(TrainUser.class);
+        controller = mock(TrainController.class);
+        sensor = new TrainSensorImpl(controller,user);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void noAlarmTest() {
+        when(controller.getReferenceSpeed()).thenReturn(50);
+        sensor.overrideSpeedLimit(100);
+        assertFalse(sensor.getAlarmStatus());
+    }
+
+    @Test
+    public void negativeLimitTest() {
+        when(controller.getReferenceSpeed()).thenReturn(50);
+        sensor.overrideSpeedLimit(-100);
+        assert (sensor.getAlarmStatus());
+    }
+
+    @Test
+    public void limitAbove500Test() {
+        when(controller.getReferenceSpeed()).thenReturn(50);
+        sensor.overrideSpeedLimit(600);
+        assert (sensor.getAlarmStatus());
+    }
+
+    @Test
+    public void relativeLimitAlarmTest() {
+        when(controller.getReferenceSpeed()).thenReturn(150);
+        sensor.overrideSpeedLimit(50);
+        assert (sensor.getAlarmStatus());
     }
 }
