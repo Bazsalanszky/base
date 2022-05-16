a57b5dcc69a0381c00d7f3ac3169a152796f4916
Bazsalanszky <balazs@toldi.eu>
2022-05-16 16:55:58 +0200

    Alarm feature

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -6,24 +6,36 @@ import hu.bme.mit.train.interfaces.TrainUser;
 
 public class TrainSensorImpl implements TrainSensor {
 
-	private TrainController controller;
-	private TrainUser user;
-	private int speedLimit = 5;
+    private TrainController controller;
+    private TrainUser user;
+    private int speedLimit = 5;
 
-	public TrainSensorImpl(TrainController controller, TrainUser user) {
-		this.controller = controller;
-		this.user = user;
-	}
+    private boolean alarm;
 
-	@Override
-	public int getSpeedLimit() {
-		return speedLimit;
-	}
+    public TrainSensorImpl(TrainController controller, TrainUser user) {
+        this.controller = controller;
+        this.user = user;
+    }
 
-	@Override
-	public void overrideSpeedLimit(int speedLimit) {
-		this.speedLimit = speedLimit;
-		controller.setSpeedLimit(speedLimit);
-	}
+    @Override
+    public int getSpeedLimit() {
+        return speedLimit;
+    }
+
+    @Override
+    public void overrideSpeedLimit(int speedLimit) {
+        if (speedLimit < 0 || speedLimit > 500 || controller.getReferenceSpeed() / 2 > speedLimit) {
+            alarm = true;
+            return;
+        }
+        this.speedLimit = speedLimit;
+        controller.setSpeedLimit(speedLimit);
+    }
+
+    @Override
+    public boolean getAlarmStatus() {
+        return alarm;
+    }
+
 
 }
